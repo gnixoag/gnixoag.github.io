16d69710f0ee4be9e3cdc2ea4d21473657c28a10
gnix_oag <gnix.oag@gmail.com>
2019-12-01 21:53:39 +0800

    正常试用中

---
+++ content/pages/about.md
@@ -0,0 +1,26 @@
+title: 作者
+Date: 2019-12-03 10:20
+Slug: author
+
+*[·-·]: Tags: 总结, 工作, 学习
+*[·-·]: Author: 高星
+*[·-·]: Summary: 
+*[·-·]:Modified: 2010-12-05 19:30
+*[@-@]: keywords authors lang translation status template save_as url
+*[·-·]: Category: Python
+
+
+
+#### 职称
+  - 副教授
+#### 学位
+  - 硕士学位
+#### 研究方向
+  - 数控技术
+  - CAD/CAM
+  - 逆向工程
+  - 制造业信息化
+#### 兴趣
+  - Linux
+  - C++/QT
+  - Python
--- pelicanconf.py
+++ pelicanconf.py
@@ -8,27 +8,36 @@ SITENAME = '高星的博客'
 
 PATH = 'content'
 
+#插件
 PLUGIN_PATHS = ['D:\\webpage\\pelican-plugins']
 PLUGINS = ['i18n_subsites']
+
+#主题设置
 THEME = 'D:\\webpage\\pelican-themes\\pelican-bootstrap3'
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 I18N_TEMPLATES_LANG = 'en'
 BOOTSTRAP_THEME =  'darkly'
-#BOOTSTRAP_THEME =  'abc'
+
+#显示文章作者、分类、修改日期
 SHOW_ARTICLE_AUTHOR = True
 SHOW_ARTICLE_CATEGORY = True
 SHOW_DATE_MODIFIED = True
+
+#代码显示风格
 PYGMENTS_STYLE =  'vim'
-#BOOTSTRAP_NAVBAR_INVERSE = True
+
+
 DISPLAY_TAGS_ON_SIDEBAR = True
 DISPLAY_CATEGORIES_ON_SIDEBAR = True
 DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
-#DISPLAY_ARCHIVE_ON_SIDEBAR = True
-#DISPLAY_AUTHORS_ON_SIDEBAR  = True
+DISPLAY_ARCHIVE_ON_SIDEBAR = True
+DISPLAY_AUTHORS_ON_SIDEBAR  = True
 
 USE_FOLDER_AS_CATEGORY = True
 DISPLAY_PAGES_ON_MENU = True
+
 DISPLAY_CATEGORIES_ON_MENU = True
+
 GITHUB_URL = 'http://gnixoag.github.io'
 
 
@@ -49,9 +58,10 @@ LINKS = (('国内博客', 'https://gnix_oag.coding.me/home/'),
          ('工作单位', 'http://www.hnxxjsxy.com'),)
 
 # Social widget
-SOCIAL = (('Email', 'mailto:gnix.oag@gmail.com'),)
+SOCIAL = (('Email', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+
